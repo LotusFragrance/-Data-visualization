8dfe5274c7fe85625c5b8448aeba5c6d0f9bb82a
Lotus_fragrance <2369275200@qq.com>
2022-11-19 17:23:57 +0800

    项目外观初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,12 @@ module.exports = {
     '@vue/standard'
   ],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- 基本布局 -->
+  <div class="viewport">
+    <div class="column">
+      <div class="pannel">
+        <div class="inner">11
+          11
+          11
+          <br>
+        </div>
+      </div>
+    </div>
+    <div class="column">
+      <div class="middle">2</div>
+    </div>
+    <div class="column">3</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default {}
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+.viewport {
+  padding: 88px 20px 0;
+  display: flex;
+  background: url(@/assets/images/logo.png) no-repeat 0 0 / contain;
+  .column {
+    flex: 3;
+    color: #fff;
+    &:nth-child(2) {
+      flex: 4;
+      padding: 30px 15px 0;
+      background-color: transparent;
+      .middle {
+        color: #fff;
+      }
+    }
+  }
 }
 </style>
